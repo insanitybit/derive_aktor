eb4dedfece9e3117e184d9cd640d9e3a9bfe618a
insanitybit <insanitybit@gmail.com>
2017-04-30 16:12:18 -0400

    Remove testderive target

--- testderive/target/debug/libtestderive.d
+++
@@ -1,1 +0,0 @@
-/home/indie/workspace/derive-aktor/testderive/target/debug/libtestderive.rmeta: /home/indie/workspace/derive-aktor/src/lib.rs /home/indie/workspace/derive-aktor/testderive/src/main.rs
--- testderive/target/debug/testderive.d
+++
@@ -1,1 +0,0 @@
-/home/indie/workspace/derive-aktor/testderive/target/debug/testderive: /home/indie/workspace/derive-aktor/testderive/src/main.rs /home/indie/workspace/derive-aktor/src/lib.rs
