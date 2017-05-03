f2d60cb4846494c017cce4a21208af899c82b6f8
insanitybit <insanitybit@gmail.com>
2017-05-03 15:03:46 -0400

    Remove target... again.

--- testderive/target/debug/libtestderive.d
+++
@@ -1,1 +0,0 @@
-/home/indie/workspace/derive_aktor/testderive/target/debug/libtestderive.rmeta: /home/indie/workspace/derive_aktor/testderive/src/main.rs /home/indie/workspace/derive_aktor/src/lib.rs
