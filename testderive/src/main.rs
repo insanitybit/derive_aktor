5b08d828e4da5972b71dad9987607beb8755898d
insanitybit <insanitybit@gmail.com>
2017-05-06 12:03:33 -0400

    Update example

--- testderive/src/main.rs
+++ testderive/src/main.rs
@@ -6,26 +6,36 @@
 #[macro_use]
 extern crate derive_aktor;
 
+use std::fmt::Debug;
 
 use derive_aktor::derive_actor;
+use fibers::{Executor, ThreadPoolExecutor};
 
-pub struct Foo<A: 'static + Send, B: 'static + Send>{
-    bar: A,
-    baz: B
+pub struct PrintLogger {
+
 }
 
-
-
 #[derive_actor]
-impl<A: 'static + Send, B: 'static + Send> Foo<A, B> {
+impl PrintLogger {
+    pub fn info<T: Debug + Send + 'static>(&self, data: T) {
+        println!("{:?}", data);
+    }
 
-    pub fn bar<T: 'static, U>(&self, baz: u32, blah: T, blahh: U) -> bool
-        where U: 'static {
-        true
+    pub fn error<U: Debug + Send + 'static>(&self, data: U) {
+        println!("{:?}", data);
     }
 }
 
 fn main() {
+    let system = ThreadPoolExecutor::with_thread_count(2).unwrap();
+    let logger = PrintLogger{};
+
+    let log_actor = PrintLoggerActor::new(system.handle(), logger);
+
+    log_actor.info("info log");
+    log_actor.error("error!!".to_owned());
+
+    system.run();
 }
 
 #[test]
